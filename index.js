d8d525ea0791804de3a64bcb64f1d8133f4ad294
Sahidulislam05 <sahidulislamcst@gmail.com>
2026-01-10 20:53:04 +0600

    Added Payment API

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://civil-report.vercel.app"],
+    origin: [
+      "https://civil-report-bd.vercel.app",
+      "https://civil-report.vercel.app",
+    ],
     credentials: true,
   })
 );
